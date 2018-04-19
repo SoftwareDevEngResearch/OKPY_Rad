7bce6e78b9272596879cc63236552e37e2446bd3
Mitch Mannino <manninom@oregonstate.edu>
2018-04-19 11:46:53 -0700

    Created bit_chop and pipeout_assemble functions

--- OK_Funcs.py
+++ OK_Funcs.py
@@ -8,10 +8,17 @@ the software to properly test
 """
 
 """
-bitchop function
-Takes 
+bit_chop function
+Takes decimal number and converts it into binary. User selects which bits are 
+desired and returns a decimal number.
+
+Data: Full decimal number
+msb: Most significant bit of desired number
+lsb: least signficant bit of desired number
+Total Bits: Totals bits of signal that is being read. Related to Opal Kelly's
+wireout data. Number of bits should be established in VHDL. 
 """
-def bitchop(Data, msb, lsb, Total_Bits):
+def bit_chop(Data, msb, lsb, Total_Bits):
     Buffer_bits = str(bin(Data)[2:])
     Reverse_bits = str(Buffer_bits[::-1])
     Remaining_bits = Total_Bits - len(Reverse_bits)
@@ -20,3 +27,26 @@ def bitchop(Data, msb, lsb, Total_Bits):
     output = str(Reverse_bits[lsb:msb+1])
     
     return int(output[::-1],2)
+
+"""
+Pipeout assemble function
+Applies Opal Kelly's PipeOut read function and assembles the data 
+into the appropriate array. 
+Data: PipeRead data from the Opal Kelly Function
+Bytes: Number of Bytes in each read (4 bytes for current FPGA board)
+
+Returns array of assembled data
+"""
+def Pipeout_Assemble(Data, Bytes):
+    Buffer = bytes(Data)
+    Buffer_Reverse = Buffer[::-1]
+    Samples = len(Buffer)/Bytes #Should correspond to how many bytes are in each pipe out read
+
+    Output_Reversed = [] #Bytes had to be reversed to correctly translate bytes
+
+    for i in range(Samples):
+        Output_Reversed.append(int(Buffer_Reverse[(i*4):4+(i*4)].encode('hex'),16))
+    result = Output_Reversed[::-1]
+    return result
+
+
