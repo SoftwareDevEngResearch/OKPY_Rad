cd7342ecd891acf65f4cb560e604f940c6572af3
Mitch Mannino <manninom@oregonstate.edu>
2018-04-25 11:19:55 -0700

    Added connect_device function and allows user to select bit file.

--- Ok_Funcs.py
+++ Ok_Funcs.py
@@ -8,24 +8,33 @@ An error will occur if the appropriate package is not imported.
 
 """
 import ok
-import tkinter
+from tkinter import *
+from tkFileDialog import askopenfilename, asksaveasfilename
 
-
-class OSU_Rad_Device(device):
+class OSU_Rad_Device():
     """
     Class to designate FPGA systems at Oregon State University to connect them
     via the USB.
     """
 
     def __init__(self):
-        self.device = device
+        #self.device = self.connect_device()
+        pass
 
     """
     connect_device:
-    Simple function to create an object of the connected FPGA device. Will use This
+    Simple function to create an object of the connected FPGA device. Opens
+    Tkinter window for user to select Bit_File to program the FPGA. The window
+    then closes and programs the FPGA. Also checks for errors.
 
     """
     def connect_device(self):
-        xem = ok.okCFrontPanel()
-        xem.OpenBySerial("")
-        return xem
+        self.xem = ok.okCFrontPanel()
+        self.xem.OpenBySerial("")
+        root = Tk()
+        root.update()
+        Bit_File = askopenfilename()
+        root.update()
+        root.destroy()
+        self.xem.ConfigureFPGA(str(Bit_File))
+        return None
